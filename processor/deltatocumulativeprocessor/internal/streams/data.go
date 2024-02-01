3c45e9fa9496c9ffb70e7bcb7291171d48172d92
sh0rez <me@shorez.de>
2024-02-02 00:48:39 +0100

    draft

--- processor/deltatocumulativeprocessor/internal/streams/data.go
+++ processor/deltatocumulativeprocessor/internal/streams/data.go
@@ -9,14 +9,32 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatautil"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/data"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor/internal/metrics"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 )
+
+// Iterator as per https://go.dev/wiki/RangefuncExperiment
+type Iter[V any] func(yield func(Ident, V) bool)
+
+func Test[D data.Point[D]](m metrics.Data[D]) Iter[D] {
+	mid := m.Ident()
+
+	return func(yield func(Ident, D) bool) {
+		for i := 0; i < m.Len(); i++ {
+			dp := m.At(i)
+			id := Identify(mid, dp.Attributes())
+			if !yield(id, dp) {
+				break
+			}
+		}
+	}
+}
 
 func Samples[D data.Point[D]](m metrics.Data[D], fn func(id Ident, dp D)) {
 	mid := m.Ident()
 
 	for i := 0; i < m.Len(); i++ {
 		dp := m.At(i)
-		id := Ident{metric: mid, attrs: pdatautil.MapHash(dp.Attributes())}
+		id := Identify(mid, dp.Attributes())
 		fn(id, dp)
 	}
 }
@@ -34,4 +52,8 @@ func Update[D data.Point[D]](m metrics.Data[D], aggr Aggregator[D]) error {
 	})
 
 	return errs
+}
+
+func Identify(metric metrics.Ident, attrs pcommon.Map) Ident {
+	return Ident{metric: metric, attrs: pdatautil.MapHash(attrs)}
 }
